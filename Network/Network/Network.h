c8d19409c4bdf08110c39f0ed2525382a264e1ee
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-03-15 13:06:17 -0400

    Add files via upload

---
+++ Network/Network/Network.h
@@ -0,0 +1,15 @@
+//
+//  Network.h
+//  Network
+//
+//  Created by Student on 3/15/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Network : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Network/Network/Network.m
@@ -0,0 +1,17 @@
+//
+//  Network.m
+//  Network
+//
+//  Created by Student on 3/15/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+#import "Network.h"
+
+@implementation Network
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
